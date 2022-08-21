10666d19e1847d0a070051e36b69e52dd41faac3
sh1ple <111702486+sh1ple@users.noreply.github.com>
2022-08-22 04:38:00 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import date, datetime
 import math
+from zhdate import ZhDate as lunar_date
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
@@ -9,7 +10,9 @@ import random
 today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
+birthday = os.environ['DATE_YEAR'],
+commemoration_day = os.environ['COMM_DATE']
+
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
@@ -17,23 +20,54 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
-
+# 当地时间
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  high = math.floor(weather['high'])
+  if high >= 30 :
+    high = str(high + "今天天气很热，注意防暑防晒哦！")
+  return weather['weather'], math.floor(weather['temp']), high
 
+
+
+# 在一起的时间
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
   return delta.days
 
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+# 周年纪念日
+def get_commemorationDay():
+  next = datetime.strptime(str(date.today().year) + "-" + commemoration_day, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
 
+
+#生日
+def get_birthday(year):
+  day = lunar_date(year, 6, 1)
+  birthday = str(day.to_datetime().strftime("%m-%d"))
+  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  if (next - today).days == 0 :
+    year = int(today.year - year)
+  return (next - today).days
+
+# 年份顺延
+def get_year(birthday=birthday):
+  year = int(datetime.strptime(birthday, "%Y-%m-%d").strftime("%Y"))
+  day = lunar_date(year, 6, 1)
+  birthday = str(day.to_datetime().strftime("%m-%d"))
+  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  if next < datetime.now():
+    num = date.today().year - year
+    year = year + num + 1
+    return get_birthday(year)
+
+#文字
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
   if words.status_code != 200:
@@ -47,7 +81,14 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+wea, temperature, high = get_weather()
+data = {"weather" : {"value" : wea},
+        "temperature" : {"value" : temperature},
+        "high" : {"value" : high},
+        "love_days" : {"value" : get_count()},
+        "commemoration_days" : {"value" : get_commemorationDay()},
+        "birthday_left" : {"value" : get_year()},
+        "words":{"value" : get_words(), "color" : get_random_color()}
+        }
 res = wm.send_template(user_id, template_id, data)
 print(res)
