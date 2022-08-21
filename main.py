7b6159f00fb2de20f1828c165ac7f3f7f815d595
sh1ple <111702486+sh1ple@users.noreply.github.com>
2022-08-22 04:55:42 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ import random
 today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
-birthday = os.environ['DATE_YEAR'],
+birthday = os.environ['BIRTHDAY']
 commemoration_day = os.environ['COMM_DATE']
 
+# start_date = "2019-07-03"
+# city = "赣州"
+# birthday = "2019-06-01"
+# commemoration_day = "07-03"
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
@@ -46,26 +50,27 @@ def get_commemorationDay():
 
 
 #生日
-def get_birthday(year):
-  day = lunar_date(year, 6, 1)
-  birthday = str(day.to_datetime().strftime("%m-%d"))
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  if (next - today).days == 0 :
-    year = int(today.year - year)
+def get_birthday(year, month, day):
+  days = lunar_date(year, month, day)
+  birthday = str(days.to_datetime().strftime("%m-%d"))
+  next = datetime.strptime(str(year) + "-" + birthday, "%Y-%m-%d")
   return (next - today).days
 
 # 年份顺延
 def get_year(birthday=birthday):
   year = int(datetime.strptime(birthday, "%Y-%m-%d").strftime("%Y"))
-  day = lunar_date(year, 6, 1)
-  birthday = str(day.to_datetime().strftime("%m-%d"))
+  month = int(datetime.strptime(birthday, "%Y-%m-%d").strftime("%m"))
+  day = int(datetime.strptime(birthday, "%Y-%m-%d").strftime("%d"))
+  # 农历转换为阳历
+  days = lunar_date(year, month, day)
+  birthday = str(days.to_datetime().strftime("%m-%d"))
+
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     num = date.today().year - year
     year = year + num + 1
-    return get_birthday(year)
+    return get_birthday(year, month, day)
+
 
 #文字
 def get_words():
