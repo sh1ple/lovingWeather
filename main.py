240f9a82ce4d3d046865aa2ee71b1e1165bf409c
sh1ple <111702486+sh1ple@users.noreply.github.com>
2022-08-22 05:41:06 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp']), math.floor(weather['high']), math.floor(weather['low'])
+  return weather['weather'], str(math.floor(weather['temp'])), math.floor(weather['high']), math.floor(weather['low'])
 
 
 
